10c517e13c021327b1a3be5d44d18ea339ede373
MarcoErice <marco.erice@gmail.com>
2017-12-22 09:05:42 +0100

    some debbug.

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -16,25 +16,26 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
+            BuildWebHost(args).Run();
+            //var host = BuildWebHost(args);
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
 
-                try
-                {
-                    // Requires using MvcMovie.Models;
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
+            //    try
+            //    {
+            //        // Requires using MvcMovie.Models;
+            //        SeedData.Initialize(services);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred seeding the DB.");
+            //    }
+            //}
 
-            host.Run();
+            //host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
